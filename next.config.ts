a12af5064c50bf3524a21ea1c9dadbe339ec14f5
Solo <213974@protonmail.com>
2025-11-14 15:58:24 -0500

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  webpack(config) {
+    // Allows importing SVGs as React components
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
